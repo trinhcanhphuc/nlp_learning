31d8f38aa1fe5ce968c165cc3f2319f9a51d33c4
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-03-22 17:57:29 +0700

    update exercise chapter 2

--- chapter_2.py
+++ chapter_2.py
@@ -316,6 +316,15 @@ def exercise():
   print(list(nltk.FreqDist(text2))[:10])
   text1.concordance('monstrous')
   text2.concordance('monstrous')
+  print("=====================================")
+  print("Exercise 10")
+  print("=====================================")
+  # Read the BBC News article: “UK’s Vicky Pollards ‘left behind’” at http://news .bbc.co.uk/1/hi/education/6173441.stm. The article gives the following statistic about teen language: “the top 20 words used, including yeah, no, but and like, account for around a third of all words.” How many word types account for a third of all word tokens, for a variety of text sources? What do you conclude about this statistic? Read more about this on LanguageLog, at http://itre.cis.upenn.edu/~myl/ languagelog/archives/003993.html.
+  text = "the top 20 words used, including yeah, no, but and like, account for around a third of all words"
+  text = nltk.Text(nltk.word_tokenize(text))
+  print(text)
+  fdist1 = nltk.FreqDist(text)
+  fdist1.plot(30)
 
 """
 A function main of program
