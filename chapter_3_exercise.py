3cad4fcccab12ca4461cf6b5a193ec86afca07bf
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-04-03 01:02:45 +0700

    update NLP Book chapter 3 exercise

--- chapter_3_exercise.py
+++ chapter_3_exercise.py
@@ -13,43 +13,43 @@ from nltk.corpus import gutenberg
 import unicodedata
 
 
-# print("=====================================")
-# print("Exercise 6")
-# print("=====================================")
-# raw = """'When I'M a Duchess,' she said to herself, (not in a very hopeful tone
-# though), 'I won't have any pepper in my kitchen AT ALL. Soup does very
-# well without--Maybe it's always pepper that makes people hot-tempered,'..."""
-# patterns = [r'[a-zA-Z]+', r'[A-Z][a-z]*', r'p[aeiou]{,2}t', r'\d+(\.\d+)?', r'([^aeiou][aeiou][^aeiou])*]', r'\w+|[^\w\s]+']
+print("=====================================")
+print("Exercise 6")
+print("=====================================")
+raw = """'When I'M a Duchess,' she said to herself, (not in a very hopeful tone
+though), 'I won't have any pepper in my kitchen AT ALL. Soup does very
+well without--Maybe it's always pepper that makes people hot-tempered,'..."""
+patterns = [r'[a-zA-Z]+', r'[A-Z][a-z]*', r'p[aeiou]{,2}t', r'\d+(\.\d+)?', r'([^aeiou][aeiou][^aeiou])*]', r'\w+|[^\w\s]+']
 
-# for pattern in patterns:
-#   nltk.re_show(pattern, raw)
+for pattern in patterns:
+  nltk.re_show(pattern, raw)
 
-# print("=====================================")
-# print("Exercise 8")
-# print("=====================================")
-# def getUrlContent(url):
-#   return request.urlopen(url).read().decode('utf8')
-# print(getUrlContent('http://nltk.org/'))
+print("=====================================")
+print("Exercise 8")
+print("=====================================")
+def getUrlContent(url):
+  return request.urlopen(url).read().decode('utf8')
+print(getUrlContent('http://nltk.org/'))
 
-# print("=====================================")
-# print("Exercise 16")
-# print("=====================================")
-# from prog import monty
-# print(monty)
+print("=====================================")
+print("Exercise 16")
+print("=====================================")
+from prog import monty
+print(monty)
 
-# print("=====================================")
-# print("Exercise 18")
-# print("=====================================")
-# raw = gutenberg.raw('melville-moby_dick.txt')
-# wh_words = set(re.findall(r'\b[wh][Wh]\w+', raw))
-# print(sorted(wh_words))
+print("=====================================")
+print("Exercise 18")
+print("=====================================")
+raw = gutenberg.raw('melville-moby_dick.txt')
+wh_words = set(re.findall(r'\b[wh][Wh]\w+', raw))
+print(sorted(wh_words))
 
-# print("=====================================")
-# print("Exercise 19")
-# print("=====================================")
-# lines = open('word_freq.txt', encoding='latin2').readlines()
-# result = [[line.split(' ')[0], int(line.split(' ')[1])] for line in lines]
-# print(result)
+print("=====================================")
+print("Exercise 19")
+print("=====================================")
+lines = open('word_freq.txt', encoding='latin2').readlines()
+result = [[line.split(' ')[0], int(line.split(' ')[1])] for line in lines]
+print(result)
 
 print("=====================================")
 print("Exercise 20")
@@ -60,9 +60,6 @@ html = urlopen(req).read().decode('utf-8')
 raw = BeautifulSoup(html, 'html.parser').find('div', id='LookingAhead').get_text()
 tokens = word_tokenize(raw)
 print(tokens)
-KEYWORD=[
-  'TONIGHT', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
-]
 pattern = r'\w+\d+[°%F]*\d*'
 r = re.compile(pattern)
 print(list(filter(r.match, tokens)))
